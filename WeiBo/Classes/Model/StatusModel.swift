2b9d4f6088611f6be4a43bfa7ce7eeacb7a714fb
金占军 <519511646@qq.com>
2020-04-07 17:17:25 +0800

    添加模型

---
+++ WeiBo/Classes/Model/StatusModel.swift
@@ -0,0 +1,26 @@
+//
+//  statusModel.swift
+//  WeiBo
+//
+//  Created by jinzhanjun on 2020/4/7.
+//  Copyright © 2020 jinzhanjun. All rights reserved.
+//
+
+import Foundation
+import YYModel
+
+class StatusModel: NSObject {
+    // 微博ID
+    var id: Int64 = 0
+    // 微博内容
+    var text: String
+    
+    init(id: Int64, text: String) {
+        self.id = id
+        self.text = text
+    }
+
+    override var description: String {
+        return yy_modelDescription()
+    }
+}
